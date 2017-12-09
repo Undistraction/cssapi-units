2a6823e2eaa1cf9b9872fb624b177dbf41f963d0
Pedr Browne <pedr.browne@gmail.com>
2017-12-09 22:24:51 +0000

    test: Add missing tests for parsing functions

--- src/__tests__/parse.js
+++ src/__tests__/parse.js
@@ -1,6 +1,9 @@
 import {
   elementsOfUnitedNumber,
+  numericPartOfUnitedNumber,
+  unitPartOfUnitedNumber,
 } from '../index';
+import { genericInvalidValues, genericNumbers } from './data';
 
 describe('elementsOfUnitedNumber', () => {
   it("throws if the value isn't a number followed by a unit", () => {
@@ -17,3 +20,45 @@ describe('elementsOfUnitedNumber', () => {
     expect(elementsOfUnitedNumber('0em')).toEqual([0, 'em']);
   });
 });
+
+describe('numericPartOfUnitedNumber', () => {
+  it("throws if the supplied value isn't a united number", () => {
+    const invalidValues = [...genericInvalidValues, ...genericNumbers];
+
+    for (const value of invalidValues) {
+      expect(() =>
+        numericPartOfUnitedNumber(value)
+      ).toThrowErrorMatchingSnapshot();
+    }
+  });
+
+  it('returns the numeric part of a united number', () => {
+    expect(numericPartOfUnitedNumber('5px')).toEqual(5);
+    expect(numericPartOfUnitedNumber('-55em')).toEqual(-55);
+    expect(numericPartOfUnitedNumber('33.6rem')).toEqual(33.6);
+  });
+});
+
+describe('unitPartOfUnitedNumber', () => {
+  it("throws if the supplied value isn't a united number", () => {
+    const invalidValues = [
+      ...genericInvalidValues,
+      ...genericNumbers,
+      'xxem',
+      'NaNem',
+    ];
+    for (const value of invalidValues) {
+      expect(() =>
+        unitPartOfUnitedNumber(value)
+      ).toThrowErrorMatchingSnapshot();
+    }
+  });
+
+  it('returns the numeric part of a united number', () => {
+    expect(unitPartOfUnitedNumber('5px')).toEqual('px');
+    expect(unitPartOfUnitedNumber('-55em')).toEqual('em');
+    expect(unitPartOfUnitedNumber('33.6rem')).toEqual('rem');
+  });
+});
+
+// unitPartOfUnitedNumber
